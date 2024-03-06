a01e33c8484fc6b891ceb98c05a1da42b8fc30f3
HanBinGer <35628064+HanBinGer@users.noreply.github.com>
2024-03-06 21:15:26 +0300

    Add files via upload

---
+++ kernel.cu
@@ -0,0 +1,70 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+cudaError_t printWithCuda(unsigned int size);
+
+__global__ void printKernel()
+{
+    int i = threadIdx.x;
+    printf("Message from kernel %d \n", i);
+}
+
+int main()
+{
+    const int size = 5;
+
+    // Add vectors in parallel.
+    cudaError_t cudaStatus = printWithCuda(size);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "printWithCuda failed!");
+        return 1;
+    }
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t printWithCuda(unsigned int size)
+{
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+
+    // Launch a kernel on the GPU with one thread for each element.
+    printKernel<<<1, size>>>();
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "printKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching printKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+Error:
+    return cudaStatus;
+}
