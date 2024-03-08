d0c24f570f61177b8a4f1d06160bb1ca49de6b45
HanBinGer <35628064+HanBinGer@users.noreply.github.com>
2024-03-08 12:12:59 +0300

    Add files via upload

---
+++ 1_task/OMP.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <omp.h>
+
+int main()
+{
+	int numberOfThreads = 6;  
+
+#pragma omp parallel num_threads(numberOfThreads)  
+	{
+		int num = omp_get_thread_num(); //получаем номер потока параллельно
+#pragma omp critical
+		{
+			std::cout << "Message from thread " << num << std::endl; //доступ к консоли последовательный
+		}
+	}
+}
